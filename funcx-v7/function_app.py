64ad5f144776e9e6312f77cd6f9835a038610950
elioevp <elioevp@gmail.com>
2025-08-25 00:35:51 -0400

    test func deploy 3d

--- funcx-v7/function_app.py
+++ funcx-v7/function_app.py
@@ -6,6 +6,7 @@ import logging
 app = func.FunctionApp()
 
 # Test deployment from GitHub Actions
+# ne test deploy
 
 from receipt_processor import blueprint as receipt_blueprint
 app.register_blueprint(receipt_blueprint)
