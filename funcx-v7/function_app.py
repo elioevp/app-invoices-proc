f993117a4caae92f10155f488f54504ff9ac78bc
elioevp <elioevp@gmail.com>
2025-08-25 10:11:40 -0400

    Test4 git function

--- funcx-v7/function_app.py
+++ funcx-v7/function_app.py
@@ -6,7 +6,7 @@ import logging
 app = func.FunctionApp()
 
 # Test deployment from GitHub Actions
-# Test deploy 3
+# Test deploy 4
 
 from receipt_processor import blueprint as receipt_blueprint
 app.register_blueprint(receipt_blueprint)
