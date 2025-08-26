dfb9dff3aff94bec776ab36294b4f3a527efb42f
elioevp <elioevp@gmail.com>
2025-08-26 00:57:20 -0400

    Test10 deploy func con Gem

--- funcx-v7/function_app.py
+++ funcx-v7/function_app.py
@@ -6,7 +6,6 @@ import logging
 app = func.FunctionApp()
 
 # Test deployment from GitHub Actions
-# Test deploy 10
 
 from receipt_processor import blueprint as receipt_blueprint
 app.register_blueprint(receipt_blueprint)
